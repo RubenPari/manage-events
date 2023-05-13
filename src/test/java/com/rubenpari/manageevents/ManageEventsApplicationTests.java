a9819d7e201d40af8f12416d5e47fe3745617978
RubenPari <rubenpari.developer@gmail.com>
2023-05-13 16:06:34 +0200

    Initial commit

---
+++ src/test/java/com/rubenpari/manageevents/ManageEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubenpari.manageevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
