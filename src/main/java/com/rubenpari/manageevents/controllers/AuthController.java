e053c12e9f73071fb835bbcf82e0f077254f06a9
RubenPari <rubenpari.developer@gmail.com>
2023-05-18 13:48:49 +0200

    cleared AuthController

--- src/main/java/com/rubenpari/manageevents/controllers/AuthController.java
+++ src/main/java/com/rubenpari/manageevents/controllers/AuthController.java
@@ -1,17 +1,9 @@
 package com.rubenpari.manageevents.controllers;
 
-import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
-import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
 import com.rubenpari.manageevents.config.ResponseObject;
 import com.rubenpari.manageevents.utils.Status;
-import io.github.cdimascio.dotenv.Dotenv;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,50 +12,94 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
-import java.util.Collections;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/auth")
 public class AuthController {
-    private final Dotenv env;
-    private final AuthorizationCodeFlow authorizationCodeFlow;
+    @Value("${google.clientId}")
+    private String clientId;
 
-    public AuthController() {
-        HttpTransport httpTransport = new NetHttpTransport();
-        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+    @Value("${google.clientSecret}")
+    private String clientSecret;
 
-        this.env = Dotenv.load();
-
-        this.authorizationCodeFlow = new GoogleAuthorizationCodeFlow.Builder(httpTransport, jsonFactory, env.get("CLIENT_ID"), env.get("CLIENT_SECRET"), Collections.singletonList(env.get("SCOPES"))).build();
-    }
+    @Value("${google.redirectUri}")
+    private String redirectUri;
 
     @GetMapping(value = "/login")
-    public String login() {
-        AuthorizationCodeRequestUrl authorizationUrl = authorizationCodeFlow.newAuthorizationUrl().setRedirectUri(this.env.get("REDIRECT_URL"));
+    public String login(HttpSession session) {
+        String state = UUID.randomUUID().toString();
 
-        return "redirect:" + authorizationUrl.build();
+        session.setAttribute("state", state);
+
+        String url = "https://accounts.google.com/o/oauth2/auth?" +
+                "response_type=code" +
+                "&client_id=" + clientId +
+                "&redirect_uri=" + redirectUri +
+                "&scope=https://www.googleapis.com/auth/calendar" +
+                "&state=" + state;
+
+        return "redirect:" + url;
     }
 
     @GetMapping(value = "/callback")
-    public ResponseObject callback(HttpSession session, @RequestParam("code") String code) throws IOException {
-        GoogleTokenResponse tokenResponse;
+    public ResponseObject callback(HttpSession session,
+                                   @RequestParam("code") String code,
+                                   @RequestParam("state") String state) throws IOException {
+        String sessionState = (String) session.getAttribute("state");
 
-        try {
-            tokenResponse = (GoogleTokenResponse) authorizationCodeFlow.newTokenRequest(code).setRedirectUri(this.env.get("REDIRECT_URI")).execute();
-        } catch (Error e) {
-            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error to get access token");
+        if (sessionState == null || !sessionState.equals(state)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid state parameter");
         }
 
-        // save accessToken in session
-        session.setAttribute("accessToken", tokenResponse.getAccessToken());
+        session.removeAttribute("state");
 
-        return new ResponseObject("Sucessfully login", Status.OK);
+        URL url = new URL("https://accounts.google.com/o/oauth2/token");
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+
+        String requestBody = "grant_type=" + URLEncoder.encode("authorization_code", StandardCharsets.UTF_8)
+                + "&code=" + URLEncoder.encode(code, StandardCharsets.UTF_8)
+                + "&client_id=" + URLEncoder.encode(clientId, StandardCharsets.UTF_8)
+                + "&client_secret=" + URLEncoder.encode(clientSecret, StandardCharsets.UTF_8)
+                + "&redirect_uri=" + URLEncoder.encode(redirectUri, StandardCharsets.UTF_8);
+
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+        // execute request
+        OutputStream outputStream = connection.getOutputStream();
+        outputStream.write(requestBody.getBytes(StandardCharsets.UTF_8));
+        outputStream.close();
+
+        int responseCode = connection.getResponseCode();
+
+        if (responseCode != 200) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid code");
+        }
+
+        // get "access_token" value from response body
+        String responseBody = connection.getResponseMessage();
+        String[] responseParts = responseBody.split("&");
+
+        String accessToken = responseParts[0].split("=")[1];
+
+        session.setAttribute("AccessToken", accessToken);
+
+        return new ResponseObject("Successfully login", Status.OK);
     }
 
     @GetMapping(value = "/logout")
     public ResponseObject logout(HttpSession session) {
         session.removeAttribute("AccessToken");
 
-        return new ResponseObject("Sucessfully logout", Status.OK);
+        return new ResponseObject("Successfully logout", Status.OK);
     }
 }
