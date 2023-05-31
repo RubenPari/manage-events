a97d42ed2c81a6a41d0645f569860ad5ac1280e3
RubenPari <rubenpari.developer@gmail.com>
2023-05-31 14:55:12 +0200

    mapped logging for AuthController

--- src/main/java/com/rubenpari/manageevents/controllers/AuthController.java
+++ src/main/java/com/rubenpari/manageevents/controllers/AuthController.java
@@ -3,6 +3,7 @@ package com.rubenpari.manageevents.controllers;
 import com.rubenpari.manageevents.config.ResponseObject;
 import com.rubenpari.manageevents.utils.Status;
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/auth")
 public class AuthController {
@@ -34,7 +36,10 @@ public class AuthController {
 
     @GetMapping(value = "/login")
     public RedirectView login(HttpSession session) {
+        log.info("Called login");
+
         String state = UUID.randomUUID().toString();
+        log.info("generate state: " + state);
 
         session.setAttribute("state", state);
 
@@ -45,6 +50,7 @@ public class AuthController {
                 "&scope=https://www.googleapis.com/auth/calendar" +
                 "&state=" + state;
 
+        log.info("Redirecting to: " + url);
         return new RedirectView(url);
     }
 
@@ -55,7 +61,12 @@ public class AuthController {
     public ResponseObject callback(HttpSession session,
                                    @RequestParam("code") String code,
                                    @RequestParam("state") String state) throws IOException, InterruptedException, URISyntaxException {
+        log.info("Called callback");
+        log.info("code: " + code);
+        log.info("state from request: " + state);
+
         String sessionState = (String) session.getAttribute("state");
+        log.info("state from session: " + sessionState);
 
         if (sessionState == null || !sessionState.equals(state)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid state parameter");
@@ -76,6 +87,7 @@ public class AuthController {
                 ))
                 .header("Content-Type", "application/x-www-form-urlencoded")
                 .build();
+        log.info("Sending request to: " + url + " to get access token");
 
         // send request
         HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
@@ -84,11 +96,13 @@ public class AuthController {
         if (response.statusCode() != 200) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid code");
         }
+        log.info("Response for access token successfully received");
 
         // get "access_token" value from response body
         String accessToken = response.body().split("\"")[3];
 
         session.setAttribute("AccessToken", accessToken);
+        log.info("Access token saved in session: " + accessToken);
 
         return new ResponseObject("Successfully login", Status.OK);
     }
